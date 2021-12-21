e8ff5c481e6e3134e8b6c52980baa9db5e7ddce0
Alexandr Rebikov <rebikovlk@mail.ru>
2021-12-21 17:28:43 +0700

    test

---
+++ src/js/__tests__/validate.test.js
@@ -0,0 +1,9 @@
+import validate from '../validateCoordinates';
+
+test.each([
+  ['validate input', '51.50851, -0.12572', true],
+  ['validate input', '51.50851,-0.12572', true],
+  ['validate input', '[51.50851, -0.12572]', false],
+])(('validate input'), (_, input, expected) => {
+  expect(validate(input)).toBe(expected);
+});
