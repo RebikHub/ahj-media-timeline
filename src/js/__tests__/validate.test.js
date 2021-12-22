1b5417275b637bd31cb016b8d8b1f92fb688943a
Alexandr Rebikov <rebikovlk@mail.ru>
2021-12-22 13:45:43 +0700

    npm&jest ok

--- src/js/__tests__/validate.test.js
+++ src/js/__tests__/validate.test.js
@@ -4,6 +4,7 @@ test.each([
   ['validate input', '51.50851, -0.12572', true],
   ['validate input', '51.50851,-0.12572', true],
   ['validate input', '[51.50851, -0.12572]', true],
+  ['validate input', '[51, -0.12572]', false],
 ])(('validate input'), (_, input, expected) => {
   expect(validate(input)).toEqual(expected);
 });
--- src/js/validateCoordinates.js
+++ src/js/validateCoordinates.js
@@ -1,7 +1,7 @@
 export default function validate(coordinates) {
   const coorArr = coordinates.split(',');
-  const latitude = coorArr[0];
-  const longitude = coorArr[1];
+  const latitude = coorArr[0].trim();
+  const longitude = coorArr[1].trim();
 
   if (/^\[?-?\d{1,2}\.\d{1,9}\]?$/.test(latitude)
   && /^\[?-?\d{1,2}\.\d{1,9}\]?$/.test(longitude)) {
